b90013d059c3830326d85c5cfe6790fca503e7a9
juliethrodri24-web <juliethrodri.24@gmail.com>
2026-03-11 01:57:38 +0100

    my proyecto web

--- app.py
+++ app.py
@@ -2,4 +2,21 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df_vehiculos = pd.read_csv('vehicles_us.csv')
+df_ventas_vehiculos = pd.read_csv('vehicles_us.csv')
+hist_button = st.button('contruccion histograma')
+
+if hist_button:  # al hacer clkic el boton
+    # escribe un mensaje
+    st.write('creacion histograma para df ventas vehiculos')
+    # crear un histogramna
+    fig = px.histoframa(df_ventas_vehiculos, x="odometer")
+    # mostrar grafico en plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+# creo nuevo boton que al hacer clic crea un grafico de dispersion
+# scatter_button = st_button('construir grafico de dispersion')
+
+# if scatter_button:
+ #   st.write(
+  #      'creacxion de grafico de dispersion para el DF de anuncios de ventas de coches')
+   # fig = px.scatter(df_ventas_vehiculos, x='odometer', y='price')
+    # st.plotly_chart(fig, use_container_width=True)
