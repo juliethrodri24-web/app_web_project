0d8b622fa95f1c77ddac65d1edbfb31eef2b6e6f
juliethrodri24-web <juliethrodri.24@gmail.com>
2026-03-11 02:10:48 +0100

    my proyecto web

--- app.py
+++ app.py
@@ -7,16 +7,15 @@ hist_button = st.button('contruccion histograma')
 
 if hist_button:  # al hacer clkic el boton
     # escribe un mensaje
-    st.write('creacion histograma para df ventas vehiculos')
+    st.write('creacion histograma para la columna de odometro del DF ventas vehiculos')
     # crear un histogramna
-    fig = px.histoframa(df_ventas_vehiculos, x="odometer")
-    # mostrar grafico en plotly interactivo
+    fig = px.histogram(df_ventas_vehiculos, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 # creo nuevo boton que al hacer clic crea un grafico de dispersion
-# scatter_button = st_button('construir grafico de dispersion')
+scatter_button = st.button('construir grafico de dispersion ')
 
-# if scatter_button:
- #   st.write(
-  #      'creacxion de grafico de dispersion para el DF de anuncios de ventas de coches')
-   # fig = px.scatter(df_ventas_vehiculos, x='odometer', y='price')
-    # st.plotly_chart(fig, use_container_width=True)
+if scatter_button:
+    st.write(
+        'creacxion de grafico de dispersion:  Precio vs. Odometro')
+    fig = px.scatter(df_ventas_vehiculos, x='odometer', y='price')
+    st.plotly_chart(fig, use_container_width=True)
