8438361657a49fbc477800c3b1edf6c7628050e8
juliethrodri24-web <juliethrodri.24@gmail.com>
2026-03-20 02:40:11 +0100

    my web mejoramiento del codigo

--- app.py
+++ app.py
@@ -20,17 +20,28 @@ if scatter_button:
     fig = px.scatter(df_ventas_vehiculos, x='odometer', y='price')
     st.plotly_chart(fig, use_container_width=True)
 
-# creo casillas de verificacion
-build_histogram = st.checkbox('construir histograma')
-build_scatter = st.checkbox('construir grafico de dispersion')
+
+st.header('Analisis exploratorio de datos de vehiculos')
+col1, col2 = st.columns(2)
+
+with col1:
+    build_histogram = st.checkbox('construir histograma')
+with col2:
+    build_scatter = st.checkbox('construir grafico de dispersion')
 
 if build_histogram:
-    st.write('histograma para el conjunto de datos de anuncios de ventas de coches')
-    fig = px.scatter(df_ventas_vehiculos, x='odometer')
-    st.plotly_chart(fig, use_container_width=True)
+    st.subheader('Distribucion del kilometraje (odometer)')
+    # agrego etiquetas mas claras
+    fig_hist = px.histogram(df_ventas_vehiculos, x='odometer',
+                            title="Frecuencia del kilometraje",
+                            labels={'odometer': 'kilometraje'})
+    st.plotly_chart(fig_hist, use_container_with=True)
 
 if build_scatter:
-    st.write(
-        'creacion grafico de dispersion para el conjunto de datos de anuncios de ventas de coches')
-    fig = px.scatter(df_ventas_vehiculos, x='odometer', y='price')
-    st.plotly_chart(fig, use_container_width=True)
+    st.subheader('Relacion: Kilometraje vs. Precio')
+    # agrego etiquetas mas claras
+    fig_scatter = px.scatter(df_ventas_vehiculos, x='odometer', y='price',
+                             title="kilometraje frente a precio de venta",
+                             labels={'odometer': 'kilometraje', 'price': 'precio($)'})
+    st.plotly_chart(fig_scatter, use_container_with=True)
+    print(ig_scatter)
